330665ff2f779203a6c20220742ed0502858917a
Gaturama <bielsobv@gmail.com>
2023-04-24 09:30:30 -0300

    adição de comandos

---
+++ index.cs
@@ -0,0 +1,13 @@
+using System.Windows.Forms;
+using Views;
+
+namespace Almo
+{
+    static class Almoxarifado
+    {
+        public static void Main(string[] args)
+        {
+            Almoxarifado.index();
+        }
+    }
+}
